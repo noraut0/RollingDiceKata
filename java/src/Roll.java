5126c59535d05632512aa31d54cb6afbe08ec08a
SaucePlusUltra <npigeau@myges.fr>
2021-03-12 18:40:38 +0100

    oui

--- java/src/Roll.java
+++ java/src/Roll.java
@@ -12,7 +12,26 @@ public class Roll {
     // Attributes
 
     public Roll(String formula) {
-        // TODO
+
+
+        int d = formula.indexOf("d");
+        String nbRoll = formula.substring(0,d);
+        int nbRoll2 = Integer.parseInt(nbRoll);
+
+        String diceValue = formula.substring(d+1,d+2);
+        int diceValue2 = Integer.parseInt(diceValue);
+
+        String modifier = formula.substring(d+2,d+4);
+        int modifier2 = Integer.parseInt(modifier);
+
+        System.out.println(nbRoll2);
+        System.out.println(diceValue2);
+        System.out.println(modifier2);
+
+        Roll r = new Roll(diceValue2, nbRoll2, modifier2);
+
+
+
     }
 
     public Roll(int diceValue, int nbRoll, int modifier) {
