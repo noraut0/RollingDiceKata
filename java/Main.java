e04220f7786b5164586852fe042e8622b9498eaf
SaucePlusUltra <npigeau@myges.fr>
2021-03-12 21:02:17 +0100

    oui2

--- java/Main.java
+++ java/Main.java
@@ -7,7 +7,16 @@ public class Main {
     public static void main(String[] args) {
         RNG.setImpl(new RNGImpl());
 
-        Roll r = new Roll("2d6-2");
-        r.makeRoll(Roll.RollType.NORMAL);
+        Roll r_formula = new Roll("2d6");
+        System.out.println(r_formula.makeRoll(Roll.RollType.NORMAL));
+
+
+        /*Roll r_normal = new Roll(6,2,2);
+        System.out.println(r_normal.makeRoll(Roll.RollType.NORMAL));*/
+
+
+
+
+
     }
 }
--- java/src/Roll.java
+++ java/src/Roll.java
@@ -1,6 +1,8 @@
 package src;
 
 
+import Random.RNG;
+
 public class Roll {
 
     public enum RollType {
@@ -9,38 +11,103 @@ public class Roll {
         DISADVANTAGE
     }
 
+    public int diceValues;
+    public int nbRolls = 1;
+    public int modifiers = 0;
+    public int value = 0;
+
     // Attributes
 
     public Roll(String formula) {
-
+        int int_modifier = 0, int_nbRoll = 1, int_diceValue;
 
         int d = formula.indexOf("d");
-        String nbRoll = formula.substring(0,d);
-        int nbRoll2 = Integer.parseInt(nbRoll);
 
-        String diceValue = formula.substring(d+1,d+2);
-        int diceValue2 = Integer.parseInt(diceValue);
+        if(formula.charAt(d) == 0){
 
-        String modifier = formula.substring(d+2,d+4);
-        int modifier2 = Integer.parseInt(modifier);
+            String s_nbRoll = formula.substring(0,d);
+            int_nbRoll = Integer.parseInt(s_nbRoll);
 
-        System.out.println(nbRoll2);
-        System.out.println(diceValue2);
-        System.out.println(modifier2);
-
-        Roll r = new Roll(diceValue2, nbRoll2, modifier2);
+        }
 
 
+
+        String s_diceValue = formula.substring(d+1,d+2);
+        int_diceValue = Integer.parseInt(s_diceValue);
+
+        if( formula.substring(d + 2) == null) {
+            System.out.println(int_nbRoll);
+
+            String s_modifier = formula.substring(d + 2);
+            int_modifier = Integer.parseInt(s_modifier);
+        }
+
+        System.out.println(int_nbRoll);
+        System.out.println(int_diceValue);
+        System.out.println(int_modifier);
+
+        diceValues = int_diceValue;
+        nbRolls = int_nbRoll;
+        modifiers = int_modifier;
 
     }
 
+
     public Roll(int diceValue, int nbRoll, int modifier) {
-        // TODO
+
+        diceValues = diceValue;
+        nbRolls = nbRoll;
+        modifiers = modifier;
+
+
     }
 
     public int makeRoll(RollType rollType) {
-        // TODO
-        return 0;
+
+
+        int i;
+
+        if(nbRolls <= 0 || diceValues <= 0){
+            return -1;
+        }
+
+        for( i = 0 ; i < nbRolls ; i++){
+
+            int val1 = 0;
+            int val2 = 0;
+
+            if( rollType == RollType.NORMAL){
+
+
+                value += RNG.random( diceValues );
+            }
+
+
+            if( rollType == RollType.ADVANTAGE){
+
+
+                val1 = RNG.random( diceValues );
+                val2 = RNG.random( diceValues );
+                value = (val1 <= val2 ? val2 : val1);
+
+            }
+
+            if( rollType == RollType.DISADVANTAGE){
+
+                val1 = RNG.random( diceValues );
+                val2 = RNG.random( diceValues );
+                value = (val1 >= val2 ? val2 : val1);
+            }
+
+        }
+        value += modifiers;
+
+        if( value < 0){
+
+            value = 0;
+        }
+
+        return value;
     }
 
 }
